5ea200c3ce76bbc1d952e569b765676f5629b84e
pynex <ila8262008@gmail.com>
2025-07-14 02:24:48 +0500

    conflicts v2

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 // SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -473,39 +471,5 @@ contract Store is  Ownable {
         FundsBlocked[creator] -=refundAmount;
 
         _refundProcess(_buyer, _id, _amount, creator, price);
-=======
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
->>>>>>> 6c470b5 (hardhat)
     }
 }
